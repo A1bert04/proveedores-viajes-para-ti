e07736c7ac8a67958f032d0d7b032ec8a2e1a877
A1bert04 <albertbru04@gmail.com>
2023-08-02 08:22:40 +0200

    Create provider done

--- src/Controller/NewProviderController.php
+++ src/Controller/NewProviderController.php
@@ -2,13 +2,53 @@
 
 namespace App\Controller;
 
+use App\Entity\Provider;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class NewProviderController extends AbstractController
 {
-    public function new(): Response
+    public function new(Request $request): Response
     {
-        return $this->render('new.html.twig');
+        $newProvider = new Provider();
+
+        $form = $this->createFormBuilder($newProvider)
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class, ['required' => true])
+            ->add('phone', TelType::class, ['required' => true])
+            ->add('active', CheckboxType::class, ['required' => false])
+            ->add('type', ChoiceType::class, [
+                'required' => true,
+                'choices' => [
+                    'Hotel' => 'Hotel',
+                    'Pista' => 'Pista',
+                    'Complemento' => 'Complemento',
+                ],
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Add'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Save the entity to the database
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($newProvider);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('/?success=true');
+        }
+
+        return $this->render('new.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
